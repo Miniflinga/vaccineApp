84fdff0f52b560a1c2374505ea36e0f15bb33b8d
Miniflinga <98257566+Miniflinga@users.noreply.github.com>
2026-01-03 23:45:01 +0100

    Initial Commit

---
+++ vaccineApp/vaccineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  vaccineAppApp.swift
+//  vaccineApp
+//
+//  Created by hannali on 2026-01-03.
+//
+
+import SwiftUI
+
+@main
+struct vaccineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
